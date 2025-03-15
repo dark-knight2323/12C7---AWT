cc791a065ce370c914b3c86ecf016b70de9952ba
dark-knight2323 <rvs.raghuram07@gmail.com>
2025-03-16 02:14:14 +0530

    added toggle button

--- 15-03-25/src/app/app.component.ts
+++ 15-03-25/src/app/app.component.ts
@@ -1,18 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  message = "Displaying list of students using String Interpolation";
+  message = "Displaying list of students using Sturctural Directives";
   students = [{ id: "1", name: "Raghuram", age: "19" }, 
-              { id: "2", name: "Varun", age: "20" }, 
-              { id: "3", name: "Sanjay", age: "21" }, 
-              { id: "4", name: "Sai", age: "22" },
-              { id: "5", name: "Nishanth", age: "23" }];
-  
+    { id: "2", name: "Varun", age: "20" }, 
+    { id: "3", name: "Sanjay", age: "21" }, 
+    { id: "4", name: "Sai", age: "22" },
+    { id: "5", name: "Nishanth", age: "23" }];
+    display :boolean  = false;
+    toggle(){
+      this.display = !this.display;
+    }
 }
